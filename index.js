d0a4b2eff68b851950826fe51569fa887fb37d04
nghiaseo <nghiaseo1111988@gmail.com>
2023-02-15 23:16:27 +0700

    add socket io

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const app= require('./app')
 const config = require('./configs/config')
-app.listen(config.port)
+const http = require('http')
+const server = http.createServer(app)
+const socket = require('socket.io')(server,{cors:{origin:'*'}})
+socket.on("connection",sk=>{
+    console.log('connected with id '+sk.id)
+    sk.on("sendDataClient",(data)=>{
+        console.log(data)
+        socket.emit("sendDataServer",{data})
+    })
+})
+
+socket.on("disconnect",()=>console.log('disconnect'))
+server.listen(config.port)
